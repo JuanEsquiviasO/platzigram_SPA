e2371c8ea410f4c9d42797dc10fcaf5f8e42cc99
Juan Esquivias <juanesquiviasotazu@gmail.com>
2016-06-07 15:32:05 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var multer  = require('multer');
+var ext = require('file-extension');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, +Date.now() + '.' + ext(file.originalname));
+  }
+});
+
+var upload = multer({ storage: storage }).single('picture');
+
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Platzigram' });
+});
+
+app.get('/signup', function (req, res) {
+  res.render('index', { title: 'Platzigram - Signup' });
+});
+
+app.get('/signin', function (req, res) {
+  res.render('index', { title: 'Platzigram - Signin' });
+});
+
+app.get('/api/pictures', function (req, res, next) {
+  var pictures = [
+    {
+      user: {
+        username: '@JuanEsquiviasO',
+        avatar: 'https://pbs.twimg.com/profile_images/378800000531623712/4ea6d6ac6d974aaaa3c003650a2fe904.jpeg'
+      },
+      url: 'office.jpg',
+      likes: 0,
+      liked: false,
+      createdAt: new Date().getTime()
+    },
+    {
+      user: {
+        username: '@JuanEsquiviasO',
+        avatar: 'https://pbs.twimg.com/profile_images/378800000531623712/4ea6d6ac6d974aaaa3c003650a2fe904.jpeg'
+      },
+      url: 'office.jpg',
+      likes: 1,
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate()-10)
+    }
+  ];
+
+  setTimeout(function () {
+    res.send(pictures);
+  }, 2000);
+});
+
+app.post('/api/pictures', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.send(500, "Error uploading file");
+    }
+    res.send('File uploaded');
+  });
+});
+
+app.listen(3000, function (err) {
+  if (err) return console.log('Hubo un error'), process.exit(1);
+
+  console.log('Platzigram escuchando en el puerto 3000');
+});
